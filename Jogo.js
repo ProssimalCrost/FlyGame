b72a35f88a032cf1c77af9357decdb228059ff09
Theylon Augusto <theylon.silva@hotmail.com>
2024-11-03 21:58:43 -0300

    Add files via upload

--- Jogo.js
+++ Jogo.js
@@ -11,6 +11,13 @@ function TamanhoAutomatico() {
 TamanhoAutomatico()
 
 function posicaoRandomica() {
+
+     if(document.getElementById('mosquito')) {
+          document.getElementById('mosquito').remove()
+     }
+
+     
+
      var posicaoX = Math.floor(Math.random() * largura) - 90
      var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -21,11 +28,37 @@ function posicaoRandomica() {
 
      var mosquito = document.createElement('img')
      mosquito.src = 'imagens/mosca.png'
-     mosquito.className = 'mosquito1'
+     mosquito.className = tamanhoAleatorio() +' '+ ladoAleatorio()
      mosquito.style.left = posicaoX + 'px'
      mosquito.style.top = posicaoY + 'px'
      mosquito.style.position = 'absolute'
+     mosquito.id = 'mosquito'
 
      document.body.appendChild(mosquito)
+}
 
+function tamanhoAleatorio() {
+     var classe = Math.floor(Math.random() *  3)
+     console.log(classe)
+
+     switch(classe) {
+          case 0:
+               return 'mosquito1'
+          case 1:
+               return 'mosquito2'
+          case 2:
+               return 'mosquito3'
+     }
+}
+function ladoAleatorio() {
+     var classe = Math.floor(Math.random() *  2)
+     console.log(classe)
+
+     switch(classe) {
+          case 0:
+               return 'ladoA'
+          case 1:
+               return 'ladoB'
+
+     }
 }
