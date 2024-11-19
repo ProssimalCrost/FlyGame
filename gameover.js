90d5dd4ccaf8282e3d836a20af386a25b126c8a9
ProssimalCrost <theylon.silva@hotmail.com>
2024-11-19 14:01:23 -0300

    .

--- gameover.js
+++ gameover.js
@@ -1,12 +1,13 @@
 function tocarAudioAoCarregar() {
     document.addEventListener('DOMContentLoaded', function() {
-         const audio = new Audio('game_over_song.mp3');
+         const audio = new Audio('Audio/GameOverSong.mp3');
          audio.loop = true;
          
          audio.play().catch(error => {
               console.log('Erro ao reproduzir o áudio:', error);
          });
     });
-}
+};
 
 tocarAudioAoCarregar();
+
